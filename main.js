842d05ca5bb5f0ec16db7813f83e0857254334ae
AntonioRosales1 <elrluis213@gmail.com>
2023-05-17 17:51:25 -0700

    Colors

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const memeAuthor = document.querySelector(`.meme-generator .meme-author`);
 const updateDetails = (url, title, author) => {
   memeImage.setAttribute(`src`, url);
   memeTitle.innerHTML = title;
-  memeAuthor.innerHTML = author;
+  memeAuthor.innerHTML = `Meme by: ${author}`;
 };
 
 const generateMeme = () => {
@@ -20,3 +20,5 @@ const generateMeme = () => {
 };
 
 generateMemeBtn.addEventListener(`click`, generateMeme);
+
+generateMeme();
